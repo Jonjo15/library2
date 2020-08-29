17960ed8fcca84bb9d622264b75d8761c5be0738
IVAN <ivanbatur5g@gmail.com>
2020-08-29 10:04:38 +0200

    Finished render function

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let myLibrary = [];
+const mainDiv = document.createElement("div");
+mainDiv.classList.add("library");
+const container = document.querySelector(".container");
+container.appendChild(mainDiv);
 
 function Book(author, title, pages, readStatus) {
     this.author = author;
@@ -6,6 +10,14 @@ function Book(author, title, pages, readStatus) {
     this.pages = pages;
     this.readStatus = readStatus;
     this.index;
+    this.info = function() {
+        if (readStatus) {
+          return title + " by " + author + ", " + pages +" pages"+ ", read."
+        }
+        else {
+          return title + " by " + author + ", " + pages +" pages"+ ", not read yet."
+        }
+      }
 }
 
 function addBookToTheLibrary(book) {
@@ -14,5 +26,33 @@ function addBookToTheLibrary(book) {
 }
 
 function render() {
-
+    myLibrary.forEach((book) => {
+        const buttons = createButtons(book.index);
+        const bookDiv = document.createElement("div");
+        bookDiv.dataset.id = book.index;
+        bookDiv.classList.add("books");
+        const para = document.createElement("p");
+        para.textContent = book.info();
+        bookDiv.appendChild(para);
+        buttons.forEach((button) => {
+            bookDiv.appendChild(button);
+        })
+        mainDiv.appendChild(bookDiv);
+    });
 }
+
+function createButtons(id) {
+    const readButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+    readButton.dataset.id = id;
+    readButton.textContent = "Read"; //ovo nije gotovo
+    deleteButton.dataset.id = id;
+    deleteButton.textContent = "Delete";
+    return [readButton, deleteButton];
+}
+
+let book1 = new Book("a", "b", 3, true);
+let book2 = new Book("s", "sa", 121, false);
+addBookToTheLibrary(book1);
+addBookToTheLibrary(book2);
+render();
